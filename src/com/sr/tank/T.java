6db71a59e1e7e12d6b313f014f0ad1072ac493ed
srw <1104636252@qq.com>
2020-05-13 20:38:12 +0800

    first commit

---
+++ src/com/sr/tank/T.java
@@ -0,0 +1,16 @@
+package com.sr.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Author admin
+ * @date 2020/5/13
+ */
+public class T {
+
+    public static void main(String[] args) {
+        TankFrame tankFrame = new TankFrame();
+    }
+}
