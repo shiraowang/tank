a47e224d0e0c754d9b1e9e11a85e7117cff6e40a
srw <1104636252@qq.com>
2020-05-14 20:01:22 +0800

    处理屏幕闪烁
       Image offScreenImage = null;
        @Override
        public void update(Graphics g) {
            if (offScreenImage == null) {
                offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
            }
            Graphics gOffScreen = offScreenImage.getGraphics();
            Color c = gOffScreen.getColor();
            gOffScreen.setColor(Color.BLACK);
            gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
            gOffScreen.setColor(c);
            paint(gOffScreen);
            g.drawImage(offScreenImage, 0, 0, null);
        }

--- src/com/sr/tank/TankFrame.java
+++ src/com/sr/tank/TankFrame.java
@@ -17,11 +17,12 @@ public class TankFrame extends Frame {
 //    Dir dir = Dir.DOWN;
 //    private static final int SPEED = 10;
 
-    Tank myTank = new Tank(100,100,Dir.DOWN);
-    Bullet bullet = new Bullet(20,20,Dir.DOWN);
+    Tank myTank = new Tank(100, 100, Dir.DOWN);
+    Bullet bullet = new Bullet(20, 20, Dir.DOWN);
+    static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
     public TankFrame() {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
@@ -33,6 +34,21 @@ public class TankFrame extends Frame {
             }
         });
 
+    }
+
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     @Override
@@ -110,7 +126,6 @@ public class TankFrame extends Frame {
                     myTank.setDir(Dir.LEFT);
                 }
             }
-
 
 
         }
